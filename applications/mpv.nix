7a82244478568d87ac97b3d90c32a79d21433dff
DXC-0 <62696671+DXC-0@users.noreply.github.com>
2025-09-23 11:10:57 +0000

    Update mpv.nix

--- applications/mpv.nix
+++ applications/mpv.nix
@@ -2,17 +2,36 @@
 
 {
   home.file.".config/mpv/mpv.conf".text = ''
+    
+    # Auto-HDR / reverse-tonemaping
     vo=gpu-next
     gpu-api=vulkan
     hdr-compute-peak=yes
-    hdr-peak-detect=yes
-    target-peak=750
+    target-peak=1000
     target-prim=bt.2020
     target-trc=pq
     inverse-tone-mapping=yes
     tone-mapping=spline
-    tone-mapping-mode=auto
     target-colorspace-hint=auto
-    gamut-mapping=desaturate
+
+
+    # Basic SmoothMotion
+    video-sync=audio
+    interpolation=yes
+    tscale=sinc
+
+    # Upscaler
+    [upscaler]
+    profile-cond=width < 2160
+    scale=ewa_lanczossharp
+    cscale=ewa_lanczossharp
+    glsl-shaders-append="~/.config/mpv/shaders/KrigBilateral.glsl"
+    glsl-shaders-append="~/.config/mpv/shaders/SSimSuperRes.glsl"
+    vf="scale=iw*2:ih*2"
+    video-unscaled=no
+    deband=yes
+    deband-iterations=4
+    deband-threshold=48
+    deband-range=16
   '';
 }
